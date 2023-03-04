f60e659ede967fb9e112ce0d69db89ed156d59ce
Jay Jain <jay.jain@neyx.in>
2023-03-04 13:44:59 +0530

    basic react app added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
