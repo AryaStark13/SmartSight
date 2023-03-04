31721cccd33b4c6ef75e70433c39ecaa6a663424
jay4codes <jay4codes@gmail.com>
2023-03-04 17:06:51 +0530

    Docker deployment added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [setLoading] = useState(true);
+
+  const [values, setValues] = useState({
+    password: "",
+    username: "",
+  });
+
+  const handleChanges = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+    console.log(values);
+  };
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const history = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      username: data.get("username"),
+      password: data.get("password"),
+    });
+    createacc();
+  };
+
+  async function createacc() {
+    try {
+      let result = await fetch("http://127.0.0.1:8000/onboarding/login/", {
+        method: "POST",
+        body: JSON.stringify({
+          password: values.password,
+          username: values.username,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      result = await result.json();
+      console.log(result);
+      if (result.token) {
+        Swal.fire("Logged in Successfully!", "success", "success");
+        sessionStorage.setItem("name", result.user.username);
+        sessionStorage.setItem(
+          "user_id",
+          JSON.parse(JSON.stringify(result.user.id))
+        );
+        sessionStorage.setItem("token", result.token);
+        history("/");
+      } else {
+        Swal.fire("Oops!!", "Some error while login", "error");
+      }
+    } catch (error) {
+      console.log("Error" + error);
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div>
+      <form className="mt-5 mb-5 login-input" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            id="username"
+            label="Username"
+            name="username"
+            value={values.username}
+            onChange={handleChanges}
+            autoComplete="name"
+            autoFocus
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            name="password"
+            label="Password"
+            id="password"
+            value={values.password.trim()}
+            onChange={handleChange("password")}
+            autoComplete="current-password"
+            required
+          />
+        </div>
+        <button
+          className="btn btn-primary text-uppercase"
+          type="submit"
+          style={{ marginTop: "20px" }}
+        >
+          Login
+        </button>
+      </form>
+      <p className="mt-5 login-form__footer">
+        Dont have an account?
+        <Link to="/signup" className="text-primary">
+          Sign Up
+        </Link>
+        now
+      </p>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,131 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [setLoading] = useState(true);
+
+  const [values, setValues] = useState({
+    password: "",
+    username: "",
+    email: "",
+  });
+
+  const handleChanges = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+    console.log(values);
+  };
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const history = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      username: data.get("username"),
+      password: data.get("password"),
+      email: data.get("email"),
+    });
+    createacc();
+  };
+
+  async function createacc() {
+    try {
+      let result = await fetch("http://127.0.0.1:8000/onboarding/register/", {
+        method: "POST",
+        body: JSON.stringify({
+          password: values.password,
+          username: values.username,
+          email: values.email,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      result = await result.json();
+      console.log(result);
+      if (result.token) {
+        Swal.fire("Signed in Successfully!", "success");
+        history("/home");
+      } else {
+        Swal.fire("Oops!!", "Some error while signup", "error");
+      }
+    } catch (error) {
+      console.log("Error" + error);
+      setLoading(false);
+    }
+  }
+  return (
+    <div>
+      <form className="mt-5 mb-5 login-input" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            id="username"
+            label="Username"
+            name="username"
+            value={values.username}
+            onChange={handleChanges}
+            autoComplete="name"
+            autoFocus
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            id="email"
+            label="Email Address"
+            name="email"
+            value={values.email}
+            onChange={handleChanges}
+            autoComplete="email"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            name="password"
+            label="Password"
+            id="password"
+            value={values.password.trim()}
+            onChange={handleChange("password")}
+            autoComplete="current-password"
+            required
+          />
+        </div>
+        <button
+          className="btn btn-primary text-uppercase"
+          type="submit"
+          style={{ marginTop: "20px" }}
+        >
+          Sign in
+        </button>
+      </form>
+      <p className="mt-5 login-form__footer">
+        Already have an account?
+        <Link to="/login" className="text-primary">
+          Log In
+        </Link>
+        now
+      </p>
+    </div>
+  );
 };
 
 export default SignUp;
